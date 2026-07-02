421a75a92b03df2c444b5c4dc4220af6c936c524
itsriima <itsriima2442@gmail.com>
2026-07-02 12:04:38 +0300

    Implement Task 1 - Countdown Timer

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            //1 - Countdown Timer
+            ///////////////////////
+            Console.WriteLine("Enter a Number:");
+            int startNumber = int.Parse(Console.ReadLine());
+            for (int i = startNumber; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Liftoff!");
         }
     }
 }
