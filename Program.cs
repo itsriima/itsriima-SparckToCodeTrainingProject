40818af25fa625af248f580e607a0831d20a302a
itsriima <itsriima2442@gmail.com>
2026-07-02 10:16:09 +0300

    Implement Task 9

--- Program.cs
+++ Program.cs
@@ -130,6 +130,45 @@ namespace SparckToCodeTrainingProject
             Console.WriteLine("Original Bill : " + useramount);
             Console.WriteLine("Discount : " + discount);
             Console.WriteLine("Final Amount : " + finalAmount);
+            //9-Day Name Finder
+            //////////////////////////////
+            Console.WriteLine("Enter a number from 1 to 7:  ");
+            int day = int.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid day number");
+                    break;
+            }
 
         }
     }
