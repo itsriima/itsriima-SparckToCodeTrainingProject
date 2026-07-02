38d8d9dabca54a09174e0474eb4d8cfe29599135
itsriima <itsriima2442@gmail.com>
2026-07-02 10:11:03 +0300

    Implement Task 4

--- Program.cs
+++ Program.cs
@@ -41,6 +41,20 @@ namespace SparckToCodeTrainingProject
             {
                 Console.WriteLine("Odd Number ");
             }
+            //4-Voting Eligibility
+            ///////////////////////////////////////////
+            Console.WriteLine("enter Your Age : ");
+            int userage = int.Parse(Console.ReadLine());
+            Console.WriteLine("do you have a valid national ID (yes/no)");
+
+            if (userage >= 18 && Console.ReadLine() == "yes")
+            {
+                Console.WriteLine("You are eligible to vote.");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible to vote.");
+            }
 
         }
     }
