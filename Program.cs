b471b14bd67aa6f4d0cf8fc645225e29da0d7cad
itsriima <itsriima2442@gmail.com>
2026-07-02 10:06:06 +0300

    Implement Task 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace SparckToCodeTrainingProject
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+
+namespace SparckToCodeTrainingProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1- Personal Info Card
+            //////////////////////////////////////
+            string name = "Rima ";
+            int age = 20;
+            double height = 1.55;
+            bool student = true;
+            Console.WriteLine("Name: " + name + "Age:" + age + "Height: " + height + "Student: " + student);
         }
     }
 }
+
